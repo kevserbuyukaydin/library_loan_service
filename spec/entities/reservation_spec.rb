77018d66d4b4c33b47498ea7bcb12a4ea573db68
kevserbuyukaydin <kevserbuyukaydin@gmail.com>
2026-09-05 18:23:59 +0300

    Update Reservation tests for pending/fulfilled status

--- spec/entities/reservation_spec.rb
+++ spec/entities/reservation_spec.rb
@@ -8,14 +8,93 @@ RSpec.describe Reservation do
       reservation = Reservation.new(
               id: 1,
               isbn: "isbn-dune",
-              member_id: "m1",
+              member_id: "member-1",
               requested_on: requested_on
       )
 
       expect(reservation.id).to eq(1)
       expect(reservation.isbn).to eq("isbn-dune")
-      expect(reservation.member_id).to eq("m1")
+      expect(reservation.member_id).to eq("member-1")
       expect(reservation.requested_on).to eq(requested_on)
+      expect(reservation.pending?).to be true
+    end
+  end
+  
+    
+  describe "#pending?" do
+    it "returns true when status is pending" do
+      requested_on = Date.today
+
+      reservation = Reservation.new(
+              id: 1,
+              isbn: "isbn-dune",
+              member_id: "member-1",
+              requested_on: requested_on
+      )
+      
+      expect(reservation.pending?).to be true
+    end
+
+    it "returns false when status is fulfilled" do
+      requested_on = Date.today
+
+      reservation = Reservation.new(
+              id: 1,
+              isbn: "isbn-dune",
+              member_id: "member-1",
+              requested_on: requested_on,
+              status: :fulfilled
+      )
+      
+      expect(reservation.pending?).to be false
+    end
+  end
+
+  describe "#fulfilled?" do
+    it "returns true when status is fulfilled" do
+      requested_on = Date.today
+
+      reservation = Reservation.new(
+              id: 1,
+              isbn: "isbn-dune",
+              member_id: "member-1",
+              requested_on: requested_on,
+              status: :fulfilled
+      )
+      
+      expect(reservation.fulfilled?).to be true
+    end
+
+    it "returns false when status is pending" do
+      requested_on = Date.today
+
+      reservation = Reservation.new(
+              id: 1,
+              isbn: "isbn-dune",
+              member_id: "member-1",
+              requested_on: requested_on
+      )
+      
+      expect(reservation.fulfilled?).to be false
+    end
+  end
+
+
+
+  describe "#fulfill!" do
+    it "changes status to fulfilled" do
+      requested_on = Date.today
+
+      reservation = Reservation.new(
+              id: 1,
+              isbn: "isbn-dune",
+              member_id: "member-1",
+              requested_on: requested_on
+      )
+
+      reservation.fulfill!
+
+      expect(reservation.fulfilled?).to be true
     end
   end
 end
