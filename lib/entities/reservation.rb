fc4e83c6c338a8b24f30ac241b5757f1255cc981
kevserbuyukaydin <kevserbuyukaydin@gmail.com>
2026-09-05 16:31:08 +0300

    Add status (pending/fulfilled) to Reservation to support R6 cancel edge case

--- lib/entities/reservation.rb
+++ lib/entities/reservation.rb
@@ -1,12 +1,27 @@
 require 'date'
 
 class Reservation
-  attr_reader :id, :isbn, :member_id, :requested_on
+  STATUSES = %i[pending fulfilled].freeze
 
-  def initialize(id:, isbn:, member_id:, requested_on:)
+  attr_reader :id, :isbn, :member_id, :requested_on, :status
+
+  def initialize(id:, isbn:, member_id:, requested_on:, status: :pending)
     @id = id
     @isbn = isbn
     @member_id = member_id
     @requested_on = requested_on
+    @status = status
+  end
+
+  def pending?
+    status == :pending
+  end
+
+  def fulfilled?
+    status == :fulfilled
+  end
+
+  def fulfill!
+    @status = :fulfilled
   end
 end
